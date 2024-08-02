28900081f64997feeb827ba79434129102d72d4b
srani691 <s.rani.15@student.scu.edu.au>
2024-08-02 22:10:17 +0500

    Create constructor to initalize values

--- src/StudentMarks.java
+++ src/StudentMarks.java
@@ -13,5 +13,14 @@ public class StudentMarks
         String studentId;
         double[] marks = new double[3];
         double totalMarks;
+        public Student(String lastName, String firstName, String studentId, double a1, double a2, double a3) {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.studentId = studentId;
+            this.marks[0] = a1;
+            this.marks[1] = a2;
+            this.marks[2] = a3;
+            this.totalMarks = a1 + a2 + a3;
+        }
     }
 }
